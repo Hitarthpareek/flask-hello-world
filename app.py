7d3109af134cec0421665bbf91a09280a7c381f3
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2024-04-03 20:20:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
-
 app = Flask(__name__)
 
-@app.route('/get', methods=['POST'])
-def echo_string():
-        return '1234'
+@app.route('/')
+def hello_world():
+    return 'Hello from git'
 
+if __name__ == "__main__":
+    app.run()
